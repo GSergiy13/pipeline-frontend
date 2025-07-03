8e0459f242a8afb6afb4c4bdb09ac38f178b0d23
GSergiy13 <frodo13lot@gmail.com>
2025-07-03 20:04:11 +0300

    first commit

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,8 @@
+export const HomePage = () => {
+	return (
+		<div className='w-full h-[100vh] flex flex-col items-center justify-center'>
+			<h1>Welcome to the Home Page</h1>
+			<p>This is the main page of our application.</p>
+		</div>
+	)
+}
---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+	return (
+		<header className='w-full h-16 bg-gray-800 text-white flex items-center justify-center'>
+			<h1 className='text-xl font-bold'>Pipeline Frontend</h1>
+		</header>
+	)
+}
