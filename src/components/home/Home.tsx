d582f474a0850efc8145a52b2e0e2176f24e671f
GSergiy13 <frodo13lot@gmail.com>
2025-07-05 13:55:53 +0300

    fix: remove unnecessary height style from separator in PromptGenerateButton; clean up HomePage layout

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -10,7 +10,7 @@ export const HomePage = () => {
 	const userData = useSelector((state: RootState) => state.user)
 
 	return (
-		<div className='relative w-[calc(100vw-8px)] flex flex-col px-3 py-4 mx-auto h-[85vh] flex flex-col items-center justify-center bg-chat-gradient rounded-t-[32px]'>
+		<div className='relative w-[calc(100vw-8px)] flex flex-col px-3 py-4 mx-auto h-[85vh] items-center justify-center bg-chat-gradient rounded-t-[32px]'>
 			{userData.user ? (
 				<div className='mt-4'>
 					<h2>User Information:</h2>
