5fd20c7573870dc6a665863393baaa967539b25d
GSergiy13 <frodo13lot@gmail.com>
2025-07-05 18:36:43 +0300

    feat: enhance PromptGenerateButton with loading state and vibration feedback on click

--- src/components/СhatPromptPanel/PromptGenerateButton/PromptGenerateButton.tsx
+++ src/components/СhatPromptPanel/PromptGenerateButton/PromptGenerateButton.tsx
@@ -1,32 +1,51 @@
 import Image from 'next/image'
+import { useState } from 'react'
+import { handleVibrate } from 'utils/handleVibrate'
 
 export const PromptGenerateButton = () => {
+	const [isLoading, setIsLoading] = useState(false)
+
+	const handleClick = () => {
+		setIsLoading(true)
+		handleVibrate('light', 100)
+		setTimeout(() => {
+			setIsLoading(false)
+		}, 4000)
+	}
+
 	return (
 		<button
+			onClick={handleClick}
 			id='prompt-generate-button'
-			className='flex gap-3 items-center justify-center w-full border border-primary-blue py-3 px-6 bg-blue-bg-transparency-12 min-h-12 rounded-[40px] mt-1'
+			className='flex gap-3 items-center justify-center w-full border border-primary-blue py-[11pxs] px-6 bg-blue-bg-transparency-12 min-h-12 rounded-[40px] mt-1 hover:bg-blue-bg-transparency-16 transition-colors duration-150 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-blue-bg-transparency-12 disabled:hover:border-primary-blue/40'
 		>
-			<div className='flex items-center gap-1'>
-				<Image
-					src='/icons/coine.svg'
-					alt='coinse'
-					width={24}
-					height={24}
-				/>
-				<span className='text-sm text-primary-blue font-medium'>40</span>
-			</div>
+			{isLoading ? (
+				<span className='text-sm text-primary-blue/65 font-medium'>Please waiting...</span>
+			) : (
+				<>
+					<div className='flex items-center gap-1'>
+						<Image
+							src='/icons/coine.svg'
+							alt='coinse'
+							width={24}
+							height={24}
+						/>
+						<span className='text-sm text-primary-blue font-medium'>40</span>
+					</div>
 
-			<i className='w-[1px] h-[18px] bg-primary-blue/40'></i>
+					<i className='w-[1px] h-[18px] bg-primary-blue/40'></i>
 
-			<div className='flex items-center gap-1'>
-				<span className='text-sm text-primary-blue font-medium'>Generated</span>
-				<Image
-					src='/icons/magic-stick.svg'
-					alt='coinse'
-					width={24}
-					height={24}
-				/>
-			</div>
+					<div className='flex items-center gap-1'>
+						<span className='text-sm text-primary-blue font-medium'>Generated</span>
+						<Image
+							src='/icons/magic-stick.svg'
+							alt='magic stick'
+							width={24}
+							height={24}
+						/>
+					</div>
+				</>
+			)}
 		</button>
 	)
 }
