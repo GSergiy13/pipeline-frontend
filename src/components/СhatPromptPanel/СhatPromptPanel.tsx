4c01060081f3ce51620db83ea184cc59d46e7e40
GSergiy13 <frodo13lot@gmail.com>
2025-07-12 12:05:58 +0300

    fix: correct positioning class in ChatPromptPanel for improved layout

--- src/components/СhatPromptPanel/СhatPromptPanel.tsx
+++ src/components/СhatPromptPanel/СhatPromptPanel.tsx
@@ -4,7 +4,7 @@ import { UserInfoPanel } from './UserInfoPanel/UserInfoPanel'
 
 export const ChatPromptPanel = () => {
 	return (
-		<div className='absolute bottom-6 flex flex-col mt-auto w-full z-40 transition-all duration-150 max-w-[640px] mx-auto left-1/2 -translate-x-1/2'>
+		<div className=' relative bottom-6 flex flex-col mt-auto w-full z-40 transition-all duration-150 max-w-[640px] mx-auto left-1/2 -translate-x-1/2'>
 			<UserInfoPanel />
 			<PromptInputWrapper />
 			<PromptGenerateButton />
