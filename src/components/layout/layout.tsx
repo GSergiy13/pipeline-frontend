45b39ac86bb7baa7e3dca2f6ee05f7ff73320140
GSergiy13 <frodo13lot@gmail.com>
2025-07-14 11:07:02 +0300

    fix: update layout structure in HomePage and Layout components for improved responsiveness

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -5,14 +5,12 @@ import { Header } from './Header/Header'
 
 export const Layout = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<>
+		<div className='flex flex-col min-h-screen'>
 			<FixTelegramViewport />
 			<Header />
-			<main>{children}</main>
-			<footer>
-				<p className='pt-1 text-[10px] text-white/40 text-center'>Name AI v. 0.1 beta</p>
-			</footer>
+			<main className='flex-grow flex w-full h-full'>{children}</main>
+			<footer className='text-[10px] text-white/40 text-center pt-1'>Name AI v. 0.1 beta</footer>
 			<TelegramInitializer />
-		</>
+		</div>
 	)
 }
