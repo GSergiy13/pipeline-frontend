c9d1f1747ff923d3693c9067e5ff06f80436ee78
GSergiy13 <frodo13lot@gmail.com>
2025-07-14 11:36:19 +0300

    fix: update footer content and padding for improved layout consistency

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -1,5 +1,7 @@
 export const Footer = () => {
 	return (
-		<footer className='text-[10px] text-white/40 text-center py-1'>Name AI v. 0.11 beta</footer>
+		<footer className='text-[10px] text-white/40 text-center pt-1 pb-3'>
+			Name AI v. 0.12 beta
+		</footer>
 	)
 }
