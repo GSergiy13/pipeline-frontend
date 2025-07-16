8924c8fa3f12d6c66adcc66758857cc4398f7cfe
GSergiy13 <frodo13lot@gmail.com>
2025-07-16 14:52:10 +0300

    fix: update safe area handling in Header component for mobile compatibility

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -2,6 +2,7 @@
 
 import cn from 'clsx'
 import { modelgenerate } from 'constants/modelgenerate.const'
+import { useLayoutEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import type { RootState } from 'store/store'
 
@@ -12,17 +13,21 @@ import { MarqueeText } from './MarqueeText/MarqueeText'
 
 export const Header = () => {
 	const isMobileTelegram = useSelector((state: RootState) => state.user.isMobileTelegram)
+	const [showSafeArea, setShowSafeArea] = useState(false)
+
+	useLayoutEffect(() => {
+		setShowSafeArea(isMobileTelegram)
+	}, [isMobileTelegram])
 
 	return (
 		<header
 			className={cn(`w-full pb-3`, {
-				'tg-safe-area': isMobileTelegram
+				'tg-safe-area': showSafeArea
 			})}
 		>
 			<div className='flex flex-col pt-3'>
 				<div className='w-full flex items-center justify-between px-3 mb-2'>
 					<DropDown data={modelgenerate} />
-
 					<Balance balance={1234567} />
 				</div>
 
