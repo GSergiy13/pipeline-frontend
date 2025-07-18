49030879a0a4db5a094394714be1ad4c6bcab6d7
GSergiy13 <frodo13lot@gmail.com>
2025-07-18 17:26:26 +0300

    fix: update videoItems array to use dynamic length based on selectedParams quantity

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,10 +4,13 @@ import cn from 'clsx'
 import { VideoItem } from 'components/VideoItem/VideoItem'
 import { ChatPromptPanel } from 'components/СhatPromptPanel/СhatPromptPanel'
 import { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
+import type { RootState } from 'store/store'
 
 export const HomePage = () => {
 	const ref = useRef<HTMLDivElement>(null)
 	const [height, setHeight] = useState(150)
+	const data = useSelector((state: RootState) => state.generation.selectedParams.quantity)
 
 	useEffect(() => {
 		if (ref.current) {
@@ -15,7 +18,10 @@ export const HomePage = () => {
 		}
 	}, [])
 
-	const videoItems = [1, 3, 4, 5, 3]
+	const videoItems = Array.from(
+		{ length: typeof data === 'number' && data > 0 ? data : 0 },
+		(_, i) => i + 1
+	)
 	const videoCount = videoItems.length
 	const isCompactLayout = videoItems.length > 2
 
@@ -29,11 +35,6 @@ export const HomePage = () => {
 					' w-full overflow-y-auto min-h-[350px] max-h-full h-full',
 					videoCount > 2 ? 'grid grid-cols-2 gap-1.5' : 'flex flex-col'
 				)}
-				style={
-					{
-						// height: `calc(100%  - ${height + 26}px)`
-					}
-				}
 			>
 				{videoItems.map((item, index) => (
 					<VideoItem
