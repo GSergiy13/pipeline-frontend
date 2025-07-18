852ef37381c211aea2fbc15a654aaf1c01f6a3cc
GSergiy13 <frodo13lot@gmail.com>
2025-07-18 15:11:19 +0300

    fix: update footer component to conditionally set padding based on mobile Telegram user status

--- src/components/layout/Footer/Footer.tsx
+++ src/components/layout/Footer/Footer.tsx
@@ -7,8 +7,10 @@ export const Footer = () => {
 	const userData = useSelector((state: RootState) => state.user)
 
 	return (
-		<footer className='text-[10px] text-white/40 text-center pt-1 pb-5'>
-			Name AI v. 0.1 beta{' '}
+		<footer
+			className={`text-[10px] text-white/40 text-center pt-1  ${userData.isMobileTelegram ? 'pb-5' : 'pb-2'}`}
+		>
+			Pipeline AI v. 0.1 beta{' '}
 			{userData.user?.tg_data?.id ? `by user id ${userData.user.tg_data?.id}` : ''}
 		</footer>
 	)
