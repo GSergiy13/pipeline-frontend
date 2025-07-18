7afeab99557c3926aa7c8ed34268f8856f8e5813
GSergiy13 <frodo13lot@gmail.com>
2025-07-18 15:58:49 +0300

    fix: remove absolute positioning from inner div in HomePage component for improved layout

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -23,7 +23,7 @@ export const HomePage = () => {
 		<div className='relative flex flex-1 flex-col px-1 pt-1 mx-auto bg-chat-gradient rounded-t-[32px]'>
 			<div
 				className={cn(
-					' absolute w-full top-0 left-0 overflow-y-auto min-h-[380px]',
+					' w-full  overflow-y-auto min-h-[380px]',
 					videoCount > 2 ? 'grid grid-cols-2 gap-1.5' : 'flex flex-col'
 				)}
 				style={{
